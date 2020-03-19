4b68aaaa312163888b5353ca6e3154ebe37d4ddd
Lucas Fernando <lucas.fernandosilva@hotmail.com>
2020-03-19 11:42:37 -0300

    Update teste_sensor_de_liquido.ino

--- Arduino/teste_sensor_de_liquido/teste_sensor_de_liquido.ino
+++ Arduino/teste_sensor_de_liquido/teste_sensor_de_liquido.ino
@@ -32,28 +32,4 @@ if(estado == 1){
   }
   delay(100);
 }
-  /*
-void loop()
-{
-  int estado = digitalRead(boia);
-  Serial.print("Leitura do sensor : ");
-  Serial.println(estado);  
-  switch(estado)
-  {
-  case 0:
-    // Fica vermelho o led pois a caixa estaria cheia e a bomba seria ligada para passar a água para outro lugar.
-    Serial.println("Caixa Cheia bomba d agua ativada");
-    digitalWrite(led_vermelho, 0);
-    digitalWrite(led_verde, 1);
-    // quanto tempo a boia vai ficar ligada
-    
-    break;        
-  case 1:
-    // Fica verde o led pois a caixa estaria enchendo
-    Serial.println("Caixa enchendo bomba d agua desativada");
-    digitalWrite(led_vermelho, 1);
-    digitalWrite(led_verde, 0);
-    break;
-  }
-  delay(100);
-}*/
+ 
